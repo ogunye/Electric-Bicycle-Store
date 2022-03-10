fb60b398ded2b4aab999d0bcc31fd78fa7270a3e
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2022-03-10 15:17:26 +0100

    Project Setup and ProductAPI Setup

---
+++ Electric-Bicycle-Store.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Electric_Bicycle_Store.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
